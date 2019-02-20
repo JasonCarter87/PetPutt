563ee9df653064520d73df404230abfa4d336bbd
JasonCarter87 <35275412+JasonCarter87@users.noreply.github.com>
2019-02-20 18:46:24 -0500

    Added some pool stick functionality

---
+++ PetPutt-Updated/PetPutt/Stick.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#self.position = get_global_mouse_position()
+	self.look_at(get_global_mouse_position())
+	#$"Sprite".look_at(ball.get_position_in_parent())
+	pass
