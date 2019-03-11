78a9d85ccb4a7a24bcd2495a211b6614b3811ce9
Jason Carter Costes <carter.costes@gmail.com>
2019-03-10 21:19:14 -0400

    Removed Unnecessary files

--- PetPutt-Milestone2WIP/PetPutt/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var ballMoving = false
-var gameOver = false
-
-func _ready():
-	pass # Replace with function body.
--- PetPutt-Milestone3WIP/PetPutt/global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var ballMoving = false
-var gameOver = false
-var bambooHealth = 3
-
-func _ready():
-	pass # Replace with function body.
